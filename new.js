0a820d1b2b7a61b41c2239e27501970a399eef3f
dunnisleek <94145981+dunnisleek@users.noreply.github.com>
2022-04-25 19:23:51 +0100

    initial.commit
    updated

---
+++ new.js
@@ -0,0 +1,16 @@
+alert('helo')
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function() {
+   
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value=" ";
+  paragraph.addEventListerner('click',function(){
+      paragraph.style.textDecoration ="line-through";
+  })
+})
